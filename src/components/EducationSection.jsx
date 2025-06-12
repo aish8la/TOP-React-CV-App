cf49c0af7138b66ac8b7cef656ea02b7ec7f7dea
aish8la <64914670+aish8la@users.noreply.github.com>
2025-06-13 02:58:13 +0500

    feat(EducationSection): add function to load dialog form data

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export function EducationSection({ data }) {
+
+    const [currentForm, setCurrentForm] = useState(null);
+
     const dialogRef = "edu-dialog";
 
     function toggleDialog () {
         const dialog = document.getElementById(dialogRef);
         dialog.showModal();
     }
+
+    function loadEducationEntry(uid) {
+        const entry = data.find(entry => {
+            return entry.uid === uid;
+        });
+
+        setCurrentForm({...entry});
+
+    }
+
     return (
         <fieldset>
             <legend>
@@ -14,7 +29,10 @@ export function EducationSection({ data }) {
                 {data.map(entry => {
                     return (
                         <li key={entry.uid}
-                        onClick={toggleDialog}
+                        onClick={() => {
+                            loadEducationEntry(entry.uid);
+                            toggleDialog()
+                        }}
                         >
                             <div>
                                 {entry.level}
@@ -27,43 +45,58 @@ export function EducationSection({ data }) {
                 })}
             </ul>
             <button>Create New</button>
-            <FormDialog dialogId={dialogRef} />
+            <FormDialog dialogId={dialogRef} currentFormData={currentForm} />
         </fieldset>
     )
 }
 
-function FormDialog({ dialogId }) {
+function FormDialog({ dialogId, currentFormData }) {
+
+    let data = {};
+
+    if(!currentFormData) {
+        data.institution = ""
+        data.level = ""
+        data.location = ""
+        data.startDate = ""
+        data.endDate = ""
+        data.grade = ""
+        data.description = ""
+    } else {
+        data = {...currentFormData};
+    }
+
     return (
         //This is a bad idea, use ref for real projects but since it is not covered in TOP at this stage i am using id
         <dialog id={dialogId}>
             <form method='dialog'>
                 <label htmlFor="institution">
                     <span>Institution</span>
-                    <input type="text" id="institution" name="institution"/>
+                    <input type="text" id="institution" defaultValue={data.institution} name="institution"/>
                 </label>
                 <label htmlFor="level">
                     <span>Level</span>
-                    <input type="text" id="level" name="level"/>
+                    <input type="text" id="level" defaultValue={data.level} name="level"/>
                 </label>
                 <label htmlFor="location">
                     <span>Location</span>
-                    <input type="text" id="location" name="location"/>
+                    <input type="text" id="location" defaultValue={data.location} name="location"/>
                 </label>
                 <label htmlFor="grade">
                     <span>Grade</span>
-                    <input type="text" id="grade" name="grade"/>
+                    <input type="text" id="grade" defaultValue={data.grade} name="grade"/>
                 </label>
                 <label htmlFor="startDate">
                     <span>Start Date</span>
-                    <input type="date" id="startDate" name="startDate"/>
+                    <input type="date" id="startDate" defaultValue={data.startDate} name="startDate"/>
                 </label>
                 <label htmlFor="endDate">
                     <span>End Date</span>
-                    <input type="date" id="endDate" name="endDate"/>
+                    <input type="date" id="endDate" defaultValue={data.endDate} name="endDate"/>
                 </label>
                 <label htmlFor="description">
                     <span>Description</span>
-                    <input type="text" id="description" name="description"/>
+                    <input type="text" id="description" defaultValue={data.description} name="description"/>
                 </label>
             </form>
         </dialog>
