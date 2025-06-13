1f0f0c71e5a91ceaf1de5b3ffe726cc5d1b3ae13
aish8la <64914670+aish8la@users.noreply.github.com>
2025-06-14 01:46:36 +0500

    feat(CVPanels): add function to close and save form

--- src/components/CVPanels.jsx
+++ src/components/CVPanels.jsx
@@ -2,17 +2,31 @@ import { FormPanel } from "./CVFormPanel";
 import { CVPreviewScreen } from "./CVPreviewPanel";
 import { EducationForm } from "./EducationForm";
 import { useState } from "react";
-import { cvData } from "../logic/dataHandlers";
+import { cvData, updateData } from "../logic/dataHandlers";
 
 
 export function CVPanels() {
 
-    const [currentCVData, setCVData] = useState( {...cvData} );
+    const [currentCVData, setCurrentCVData] = useState( {...cvData} );
     const [mainScreen, setMainScreen] = useState({type: "Preview"});
+
+    function returnToPreview() {
+        setMainScreen({type: "Preview"})
+    }
 
     function showEducationForm(eduUID) {
         const entry = currentCVData.education.find(item => item.uid === eduUID);
         setMainScreen({...entry, type: "Education"});
+    }
+
+    function saveArrData(propertyName, newValue) {
+        const newData = currentCVData[propertyName].filter(item => item.uid !== newValue.uid);
+        newData.push(newValue);
+        updateData(propertyName, newValue);
+        setCurrentCVData({
+            ...currentCVData,
+            [propertyName]: newData
+        });
     }
 
 
@@ -26,6 +40,8 @@ export function CVPanels() {
             <EducationForm 
                 currentFormData={mainScreen} 
                 key={mainScreen?.uid || ""}
+                saveHandler={saveArrData}
+                closeHandler={returnToPreview}
             />
         )}
         </>
