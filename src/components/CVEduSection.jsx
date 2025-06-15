ebf5744e9968cd5cd15199370649ecffeaecf950
aish8la <64914670+aish8la@users.noreply.github.com>
2025-06-16 01:25:37 +0500

    feat: style changes for print media query

--- src/components/CVEduSection.jsx
+++ src/components/CVEduSection.jsx
@@ -15,7 +15,7 @@ export function CVEducSection({ educationData }) {
                 {educationData.map(entry => 
                     <Fragment key={entry.uid}>
                         <div className="entry-period content-title">{entry.startDate + " to " + entry.endDate}</div>
-                        <div>
+                        <div className="cv-entry-details">
                             <div className="cv-entry-titles">{entry.institution}</div>
                             <div>{entry.level}</div>
                             <div>{entry.location}</div>
